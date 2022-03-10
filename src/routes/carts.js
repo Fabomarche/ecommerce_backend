b3ec83aa1e1488a2b081723151a2c6b7e096eb7b
Fabomarche <fabriciomarche@hotmail.com>
2022-03-10 14:46:02 -0300

    whastapp de finalizada compra a admin terminado

--- src/routes/carts.js
+++ src/routes/carts.js
@@ -3,10 +3,11 @@ const router = express.Router()
 import { cartsService, userService } from '../services/services.js'
 import dotenv from 'dotenv'
 import { createTransport } from 'nodemailer'
+import twilio from "twilio"
 
 dotenv.config()
 
-
+const client = twilio(process.env.TWILIO_CLIENT_SID,process.env.TWILIO_AUTH_TOKEN)
 
 const transport = createTransport({
     service: 'gmail',
@@ -75,7 +76,6 @@ router.get('/:uid/confirm',async(req,res)=>{
         try{
             let userId = req.params.uid
             let user = await userService.getBy({_id:userId})
-            console.log(user)
             const mail = {
                 from:"Online E-commerce <Online E-commerce>",
                 to: process.env.ADMIN,
@@ -87,6 +87,14 @@ router.get('/:uid/confirm',async(req,res)=>{
             }
             let emailResult = transport.sendMail(mail)
             console.log(emailResult)
+
+            let wspResult = await client.messages.create({
+                from: "whatsapp:+14155238886",
+                to:"whatsapp:+5492215579193",
+                body:`nuevo pedido de ${user.firstName} ${user.email}, productos: ${JSON.stringify(user.cart)}`,
+            })
+            console.log(wspResult)
+            
             res.send(`Felicitaciones ${user.firstName} su compra fue realizada`)
         }catch(err){
             console.log(err)
